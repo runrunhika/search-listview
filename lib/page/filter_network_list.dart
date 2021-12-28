5913cc905f5c970b9a056b746023acd8dd121449
runrunhika <71953944+runrunhika@users.noreply.github.com>
2021-12-29 07:18:28 +0900

    first commit

---
+++ lib/page/filter_network_list.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:search_listview_sample/api/books_api.dart';
+import 'package:search_listview_sample/model/book.dart';
+import 'package:search_listview_sample/widget/search_widget.dart';
+
+import '../main.dart';
+
+class FilterNetworkListPage extends StatefulWidget {
+  @override
+  FilterNetworkListPageState createState() => FilterNetworkListPageState();
+}
+
+class FilterNetworkListPageState extends State<FilterNetworkListPage> {
+  List<Book> books = [];
+  String query = '';
+  Timer? debouncer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    init();
+  }
+
+  @override
+  void dispose() {
+    debouncer?.cancel();
+    super.dispose();
+  }
+
+  void debounce(
+    VoidCallback callback, {
+    Duration duration = const Duration(milliseconds: 1000),
+  }) {
+    if (debouncer != null) {
+      debouncer!.cancel();
+    }
+
+    debouncer = Timer(duration, callback);
+  }
+
+  Future init() async {
+    final books = await BooksApi.getBooks(query);
+
+    setState(() => this.books = books);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            buildSearch(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: books.length,
+                itemBuilder: (context, index) {
+                  final book = books[index];
+
+                  return buildBook(book);
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildSearch() => SearchWidget(
+        text: query,
+        hintText: 'Title or Author Name',
+        onChanged: searchBook,
+      );
+
+  Future searchBook(String query) async => debounce(() async {
+        final books = await BooksApi.getBooks(query);
+
+        if (!mounted) return;
+
+        setState(() {
+          this.query = query;
+          this.books = books;
+        });
+      });
+
+  Widget buildBook(Book book) {
+    return ListTile(
+        leading: Image.network(
+          book.urlImage,
+          fit: BoxFit.cover,
+          width: 50,
+          height: 50,
+        ),
+        title: Text(book.title),
+        subtitle: Text(book.author),
+      );
+  }
+}
